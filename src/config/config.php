c32216a1a22e16ee2645c2e6c3eae4dc3672f6b9
tylersriver <tyler.w.sriver@eagles.oc.edu>
2019-09-15 10:37:38 -0500

    Add sample config

---
+++ src/config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ *        Sample App Config
+ *
+ *        Your config will define the globals and defaults needed
+ *         to run your application.
+ *
+ */
+
+// MySQL Connection Info
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'test');
+define('MYSQL_PASSWORD', 'test');
+define('MYSQL_DB', 'test_db');
+
+// Defaults
+define('DEFAULT_CONTROLLER', 'pages');
+define('DEFAULT_ACTION', 'overview');
