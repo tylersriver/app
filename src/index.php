bbed6d64f4b9688c8ced84d0c525d6935008f4a9
tylersriver <tyler.w.sriver@eagles.oc.edu>
2019-11-10 10:21:41 -0600

    Convert index to app setup

--- src/index.php
+++ src/index.php
@@ -1,2 +1,40 @@
 <?php
-require_once __DIR__ . '/../ionPHP/src/index.php';
+/**
+ * Require the App class
+ */
+require_once __DIR__ . '/../ionPHP/src/IonApp.php';
+
+/**
+ * Create your app
+ */
+$app = IonApp::create([]);
+
+/**
+ * Setup the Apps DB connection
+ */
+$app->setDbInfo([
+    'host' => 'localhost',
+    'user' => 'test',
+    'password' => 'test',
+    'db' => 'test_db'
+]);
+
+/**
+ * Setup the Apps routes
+ */
+$app->setRoutes([
+    'pages' => [ 
+        'overview', 
+        'error', 
+        'login' 
+    ], 
+    'user' => [ 
+        'login', 
+        'logout' 
+    ]
+]);
+
+/**
+ * Handle the request
+ */
+$app->handle();
