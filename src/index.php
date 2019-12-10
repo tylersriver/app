f8b8ca8f806b2c76986066cdad8c5d29b1710e62
tylersriver <tyler.w.sriver@eagles.oc.edu>
2019-12-09 23:46:35 -0600

    Comments

--- src/index.php
+++ src/index.php
@@ -6,6 +6,10 @@ require_once __DIR__ . '/../ionPHP/src/IonApp.php';
 
 /**
  * Create your app
+ * 
+ * The config array passed to the create function 
+ * can originate from anywhere, create it here or have 
+ * a separate config file stored elsewhere
  */
 $app = IonApp::create([
     'default_controller' => 'view',    // Request will default to this controller if none given
@@ -15,6 +19,10 @@ $app = IonApp::create([
 
 /**
  * Setup the Apps DB connection
+ * 
+ * Simple MySQL DB Connection info, requires
+ * the following format. Again config array can be stored
+ * in a separate config file
  */
 $app->setDbInfo([
     'host' => 'localhost',
@@ -25,6 +33,10 @@ $app->setDbInfo([
 
 /**
  * Setup the Apps routes
+ * 
+ * Create a routes array for all available paths through
+ * the application, these will ALWAYS be a controller/action
+ * pair, that is how ion handles requests
  */
 $app->getRouter()->setRoutes([
     'view' => [ 
@@ -36,5 +48,7 @@ $app->getRouter()->setRoutes([
 
 /**
  * Handle the request
+ * 
+ * Final step is to handle the incoming request
  */
 $app->handle();
