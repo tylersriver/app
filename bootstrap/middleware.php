73fce36318f2f35423d707d6c72fcc7c053bdcb0
Tyler Sriver <tyler.w.sriver@eagles.oc.edu>
2023-09-04 20:30:24 -0500

    Add whoops

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -3,8 +3,10 @@
 use Limon\App;
 use Ruta\RouteMiddleware;
 use Psr\Container\ContainerInterface;
+use Franzl\Middleware\Whoops\WhoopsMiddleware;
 
 return function(App $app, ContainerInterface $c) 
 {
     $app->use($c->get(RouteMiddleware::class));
+    $app->use(new WhoopsMiddleware);
 };
